3340c43233fc700e138d301dec78341cc01dc2bf
August Nsereko-Furlong <nserekof@sheridancollege.ca>
2026-04-17 22:00:34 -0400

    Library Administration page complete. Designed the Library Administration Page

--- LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LibraryAdminPage.xaml.cs
+++ LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LibraryAdminPage.xaml.cs
@@ -12,4 +12,9 @@ public partial class LibraryAdminPage : ContentPage
     {        
         InitializeComponent();
     }
+
+    private void OnRegisterBook(object sender, EventArgs e)
+    {
+
+    }
 }
