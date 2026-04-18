ff2c9c3a9f791a15cab7f93c2e6f9be35fdb818e
August Nsereko-Furlong <nserekof@sheridancollege.ca>
2026-04-18 13:32:08 -0400

    Milestone 6 Browsing the library implemented

--- LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LibraryBrowsePage.xaml.cs
+++ LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LibraryBrowsePage.xaml.cs
@@ -6,4 +6,44 @@ public partial class LibraryBrowsePage : ContentPage
     {   
         InitializeComponent();
     }
+
+    private void OnCheckStatus(object sender, EventArgs e)
+    {
+
+    }
+    private void OnViewBookMenu(object sender, EventArgs e)
+    {
+        _viewMainMenu.IsVisible = false;
+        _viewBookMenu.IsVisible = true;
+    }
+
+    private void OnReturnToMainMenu(object sender, EventArgs e)
+    {
+        _viewMainMenu.IsVisible = true;
+        _viewBookMenu.IsVisible = false;
+    }
+
+    private void OnBorrowBook(object sender, EventArgs e)
+    {
+       
+    }
+
+    private void OnReturnBook(object sender, EventArgs e)
+    {
+       
+    }
+
+    private void OnDisplayBookAssets(object sender, EventArgs e)
+    {
+        _viewBookMenu.IsVisible = false;
+        _viewLibraryAssets.IsVisible = true;
+       // _lstBookAssets.ItemsSource = 
+    }
+
+    private void OnReturnToBookMenu(object sender, EventArgs e)
+    {
+        _viewBookMenu.IsVisible = true;
+        _viewLibraryAssets.IsVisible = false;
+        
+    }
 }
